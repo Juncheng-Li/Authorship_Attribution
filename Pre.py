198d69ddac23708aa49b38317484c01c2ae784f6
Juncheng-Li <Juncheng.alex.li@gmail.com>
2019-09-07 14:39:37 +1000

    preprocessing updated(faster

--- Pre.py
+++ Pre.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+import time
 import nltk
 from nltk import TweetTokenizer
 from nltk.corpus import stopwords
@@ -10,39 +11,49 @@ nltk.download('stopwords')
 
 
 def clean_raw_data(sentence):
-    cleaned = tt.tokenize(sentence)
-    print(cleaned)
+    tmp = tt.tokenize(sentence)
     # Convert url to domain
-    if 'http' in cleaned:
-        for i in range(len(cleaned)):
-            if 'http' in cleaned[i]:
-                cleaned[i] = tldextract.extract(cleaned[i]).domain
+    if 'http' in tmp:
+        for i in range(len(tmp)):
+            if 'http' in tmp[i]:
+                tmp[i] = tldextract.extract(tmp[i]).domain
 
     # Remove @handle
-    if '@handle' in cleaned:
+    if '@handle' in tmp:
         count = 0
-        for i in range(len(cleaned)):
-            if cleaned[i] == '@handle':
+        for i in range(len(tmp)):
+            if tmp[i] == '@handle':
                 count += 1
         for n in range(count):
-            cleaned.remove('@handle')
+            tmp.remove('@handle')
 
     # Remove stop words
     stop_words = stopwords.words('english')
     index = []
-    for i in range(len(cleaned)):
-        if cleaned[i] in stop_words:
-            index.append(cleaned[i])
+    for i in range(len(tmp)):
+        if tmp[i] in stop_words:
+            index.append(tmp[i])
     for element in index:
-        cleaned.remove(element)
+        tmp.remove(element)
 
-    return cleaned
+    cleaned_sentence = " ".join(tmp)
+    return cleaned_sentence
 
 
 df = pd.read_csv(r'./whodunnit/train_tweets.txt', names=['label', 'sentence'], sep='\t', quoting=csv.QUOTE_NONE)
 df_test = pd.read_csv(r'./whodunnit/test_tweets_unlabeled.txt', names=['label', 'sentence'], sep='\t', quoting=csv.QUOTE_NONE)
 
 tt = TweetTokenizer()
+# for n in range(len(df.sentence)):
+#     cleaned_tokens = clean_raw_data(df.sentence[n])
+#     cleaned = " ".join(cleaned_tokens)
+#     df.loc[n, 'sentence'] = cleaned
 
-cleaned = clean_raw_data('here is the @handle')
-print(cleaned)
+tic = time.clock()
+print(df)
+df['sentence'] = df['sentence'].apply(clean_raw_data)
+print(df)
+toc = time.clock()
+
+print("time processed: ")
+print(toc - tic)
